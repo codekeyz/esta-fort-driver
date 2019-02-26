53a56854efe7ea4fe50278e8868d29297ce5f55d
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-26 20:30:26 +0000

    Refactored Layout FIles

--- app/src/main/java/com/hoversoftsoln/estafortdriver/core/data/Request.kt
+++ app/src/main/java/com/hoversoftsoln/estafortdriver/core/data/Request.kt
@@ -13,10 +13,16 @@ data class Request(
         @set:PropertyName("driverID") var driverID: String = "",
         @set:PropertyName("driverName")
         @get:PropertyName("driverName") var driverName: String = "",
+        @get:PropertyName("userTelephone")
+        @set:PropertyName("userTelephone") var userTelephone: String = "",
         @get:PropertyName("date_created")
         @set:PropertyName("date_created") var dateCreated: Long = 0,
         @get:PropertyName("date_confirmed")
         @set:PropertyName("date_confirmed") var dateConfirmed: Long = 0,
+        @get:PropertyName("is_completed")
+        @set:PropertyName("is_completed") var iscompleted: Boolean = false,
+        @get:PropertyName("is_cancelled")
+        @set:PropertyName("is_cancelled") var iscancelled: Boolean = false,
         @get:PropertyName("status")
         @set:PropertyName("status") var status: Int = 0
 
